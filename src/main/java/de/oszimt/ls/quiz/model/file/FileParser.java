915f406a43639897e67de6d248fc15b7294c85d2
FloT112 <70951999+FloT112@users.noreply.github.com>
2021-11-02 11:03:01 +0100

    Add files via upload

--- src/main/java/de/oszimt/ls/quiz/model/file/FileParser.java
+++ src/main/java/de/oszimt/ls/quiz/model/file/FileParser.java
@@ -4,42 +4,49 @@ import java.io.File;
 
 import de.oszimt.ls.quiz.model.Model;
 
-public class FileParser {
-
+	private String jsonPfad;
 	private String csvPfad;
 	private String xmlPfad;
 	private XMLParser xmlParser;
+	private JSONParser jsonParser;
 
-	/**
-	 * Create FileParser
-	 * 
-	 * @param xmlPfad
-	 * @param csvPfad
-	 */
-	public FileParser(String xmlPfad, String csvPfad) {
+		/
+		* Create FileParser
+		*
+		* @param xmlPfad
+		* @param csvPfad
+		*/
+	public FileParser(String xmlPfad, String csvPfad, String jsonPfad) {
 		this.csvPfad = csvPfad;
 		this.xmlPfad = xmlPfad;
+		this.jsonPfad = jsonPfad;
 		xmlParser = new XMLParser(xmlPfad);
-	}
+		jsonParser = new jsonParser(jsonPfad);
+		}
 
-	/**
-	 * Prüft ob die XML geladen werden kann, alternativ die CSV-Datei
-	 * 
-	 * @return Model
-	 */
+		/
+		* Prüft ob die XML geladen werden kann, alternativ die CSV-Datei
+		*
+		* @return Model
+		*/
 	public Model laden() {
 		File datei = new File(xmlPfad);
+		File datei = new File(jsonPfad);
 
 		// Datei existiert
 		if (datei.exists() && datei.length() != 0) {
-			// XML laden
-			return xmlParser.laden();
+		// XML laden
+		return xmlParser.laden();
+		}
+		if (datei.exists() && datei.length() != 0) {
+		// XML laden
+		return jsonParser.laden();
 		}
 
 		// Alternativ CSV laden
 		CSVParser csvParser = new CSVParser(csvPfad);
 		return csvParser.laden();
-	}
+		}
 
 	/**
 	 * speichert alle Nutzereingaben in eine XML-Datei
@@ -48,10 +55,14 @@ public class FileParser {
 	 */
 	public void speichern(Model model) {
 		xmlParser.speichern(model);
+		jsonParser.speichern(model);
 	}
 
 	public String getXmlPfad() {
 		return xmlPfad;
+	}
+	public String getJsonPfad() {
+		return jsonPfad;
 	}
 
 }
